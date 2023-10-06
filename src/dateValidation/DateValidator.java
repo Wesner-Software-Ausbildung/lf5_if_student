332cc946b9eebf232977544b48fa1b3fccc8070e
Wesner-Software-Ausbildung <sergei.pron@wesner-software.de>
2023-10-06 16:11:13 +0200

    1. Ich versuche noch eine Lösung

--- src/dateValidation/DateValidator.java
+++ src/dateValidation/DateValidator.java
@@ -4,37 +4,50 @@ public class DateValidator {
 
     public boolean validateDate(int day, int month, int year) {
 
-        switch (day) {
-            case 1:
+        //boolean isTrue = false;
+
                 if (day >= 1 && day <= 31) {
-                    break;
-                } else if (day >= 0 && day <= 32) {
+
+                } //else if (day >= 0 && day <= 32) {
+                   // System.out.println("Das Datum ist ungültig!!");
+  //  }
+                else {
                     System.out.println("Das Datum ist ungültig!!");
-                    break;
                 }
 
-            case 2:
+
 
                 if (month >= 1 && month <= 12) {
-                    break;
-                } else if (month >= 0 && month <= 13) {
-                    System.out.println("Das Datum ist ungültig!");
-                    break;
+
                 }
+                else {
+                    System.out.println("Das Datum ist ungültig!!");
+                }
+                /*else if (month >= 0 && month <= 13) {
+                    System.out.println("Das Datum ist ungültig!");
+
+                }*/
 
 
-            case 3:
+
                 if (year >= 1 && year <= 40000) {
                     System.out.println("Das Datum ist richtig!!");
-                    break;
-                } else if (year >= 0 && year <= 40001) {
-                    System.out.println("Das Datum ist ungültig!");
-                    break;
+
+                }
+                else {
+                    System.out.println("Das Datum ist ungültig!!");
                 }
 
+                /*else if (year >= 0 && year <= 40001) {
+                    System.out.println("Das Datum ist ungültig!");
+
+                } */
+
                 if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
-                    System.out.println("Dieses Jahr ist ein Schaltjahr!");
-                    break;
+
+
+                    //System.out.println("Dieses Jahr ist ein Schaltjahr!");
+
 
                 }
 
@@ -45,7 +58,7 @@ public class DateValidator {
                 return false;
         }
 
-        return false;
+
 
     }
-}
+
